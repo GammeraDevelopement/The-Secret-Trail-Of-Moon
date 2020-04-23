2d2c004bac0b623409d3e7c51dd29f06121d9641
Hawkblade6 <61137437+Hawkblade6@users.noreply.github.com>
2020-04-23 19:46:46 +0200

    Started ScriptableObjects for levels, modified kuburi blocking

---
+++ Assets/Scripts/Mechanics/Skills/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NivelManager", menuName = "LevelManager")]
+public class LevelManager : ScriptableObject
+{
+    public Dictionary<string, Level> levels;
+
+    public LevelJSONManager levelJSONManager;
+
+    public void addExp(string name, int mechanicLevel)
+    {
+        Level nivel;
+       
+            switch (name)
+            {
+                case "Smasher":
+                levels.TryGetValue("AtencionSostenida", out nivel);
+                nivel.currentExp += levelJSONManager.getExperienciaCSAT(mechanicLevel) * 0.8f;
+                levels.TryGetValue("ControlInhibitorio", out nivel);
+                nivel.currentExp += levelJSONManager.getExperienciaCSAT(mechanicLevel) * 0.2f;
+                    break;
+                case "Kuburi":
+                    break;
+                case "TekaTeki":
+                    break;
+                case "Enigma":
+                    break;
+                case "Kitsune":
+                    break;
+
+                default:
+                    break;
+            }
+        
+
+        
+
+    }
+}
