0f81d78e6fa2f9df057a1a2663e0fb9b1f37a3a0
Yoshiki <32304200+yoshikir@users.noreply.github.com>
2020-05-06 15:07:52 +0200

    psicohud

---
+++ Assets/PsicoHUDController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsicoHUDController : MonoBehaviour
+{
+    public GameObject[] scenePrefabs;
+
+    public List<GameObject> scenePool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddToScenePool(GameObject mechanic) {
+        scenePool.Add(mechanic);
+    }
+}
---
+++ Assets/PsicoHUDSceneButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsicoHUDSceneButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeleteSceneOnClick() {
+        PsicoHUDController psico = GameObject.FindGameObjectWithTag("GameController").GetComponent<PsicoHUDController>();
+        psico.scenePool.Remove(gameObject);
+
+        //borrar del componente
+    }
+}
