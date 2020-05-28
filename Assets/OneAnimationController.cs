f5ba9c2be27ed6ea170e7f10d9fc44f4aa647239
Hawkblade6 <61137437+Hawkblade6@users.noreply.github.com>
2020-05-28 18:55:46 +0200

    Animator controller & script

---
+++ Assets/OneAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneAnimationController : MonoBehaviour
+
+{
+    private const string ANIMATION_IDLE = "IdleSit";
+    private const string ANIMATION_WALK = "Walk";
+
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool walk = false;
+
+        
+        
+    }
+
+    //TODO cuando se llame al metodo reproducir la animacion
+
+    public void Caminar() {
+
+        anim.SetBool(ANIMATION_WALK, true);
+    }
+}
