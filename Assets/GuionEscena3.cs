4da5646ff6bd7fb57808ee979919ff49d132c9f1
Hawkblade6 <61137437+Hawkblade6@users.noreply.github.com>
2020-08-11 15:35:30 +0200

    Started Scene 3

---
+++ Assets/GuionEscena3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuionEscena3 : MonoBehaviour
+{
+    public OneAnimationController oac;
+    public MoviAnimationController mac;
+
+    public MessageManager mm;
+
+    public NavMeshController navMeshOne;
+    public NavMeshController navMeshMovi;
+
+    public GameObject one;
+    public GameObject movi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
