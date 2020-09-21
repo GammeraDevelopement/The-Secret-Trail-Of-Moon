8c9f59142ae1f089d28d9d29e7b0d214ac4592f1
Hawkblade6 <61137437+Hawkblade6@users.noreply.github.com>
2020-09-21 18:36:31 +0200

    Subtitles Timeline Extension almost done

---
+++ Assets/Scenes/Adventure mode/Subtitle Timeline/SubtitleClip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SubtitleClip : PlayableAsset
+{
+    public string subtitleText;
+
+    public MessageManager mm;
+
+   /* public void gettingText()
+    {
+        subtitleText = mm.message();
+
+    }*/
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<SubtitleBehaviour>.Create(graph);
+
+        SubtitleBehaviour subtitleBehaviour = playable.GetBehaviour();
+        subtitleBehaviour.subtitleText = subtitleText;
+
+        return playable;
+    }
+}
---
+++ Assets/Scenes/Adventure mode/Subtitle Timeline/SubtitleTrack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+//Permite la creacion de un track de subtitulos custom en la timeline
+[TrackBindingType(typeof(TextMeshProUGUI))]
+[TrackClipType(typeof(SubtitleClip))]
+public class SubtitleTrack : TrackAsset
+{
+    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+    {
+        return ScriptPlayable<SubtitleTrackMixer>.Create(graph, inputCount);
+    }
+}
