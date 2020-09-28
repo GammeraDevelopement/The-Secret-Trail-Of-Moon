1c7fba629bfb2b10505973bfc217e5d65067f713
Hawkblade6 <61137437+Hawkblade6@users.noreply.github.com>
2020-09-28 17:55:17 +0200

    Started Scene2 Timeline

--- Assets/SignalEmitersEscene.cs
+++ Assets/SignalEmitersEscene.cs
@@ -20,10 +20,13 @@ public class SignalEmitersEscene : MonoBehaviour
         }
     }
     
-    //SUSTITUIR POR CASE CUANDO SE AVANCE
 
     public void NextScene()     //Carga la siguiente escena
     {
+        /*if (SceneManager.GetActiveScene().Equals(22))
+        {
+
+        }*/
 
         if (PlayerPrefs.GetInt("escena1") == 0)
         {
@@ -33,8 +36,21 @@ public class SignalEmitersEscene : MonoBehaviour
         else if (PlayerPrefs.GetInt("escena1") == 1)
         {
             PlayerPrefs.SetInt("escena1", 0);
-            SceneManager.LoadSceneAsync(21);
+            SceneManager.LoadSceneAsync(23);        //Cambiar a EXP cuando terminado (21)
         }
+
+        if (PlayerPrefs.GetInt("escena2") == 0)
+        {
+            PlayerPrefs.SetInt("escena2", 1);
+            SceneManager.LoadSceneAsync(3);
+        }
+        else if (PlayerPrefs.GetInt("escena2") == 1)
+        {
+            PlayerPrefs.SetInt("escena2", 0);
+            SceneManager.LoadSceneAsync(24);        //Cambiar a EXP cuadno terminado (21)
+        }
+
+
 
     }
 
